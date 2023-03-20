39244eb0394f913a67ab42994fbd9b6a53b20d4e
joykara <joykarani1@gmail.com>
2023-03-20 16:43:35 +0300

    create functions

--- script.js
+++ script.js
@@ -18,14 +18,31 @@ const monsterStats = document.querySelector("#monsterStats")
 const monsterName = document.querySelector("#monsterName")
 const monsterHealthText = document.querySelector("#monsterHealth")
 
+
+
 //initialize buttons
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 //create function
+function goTown() {
+    button1.innerText = "Go to Store";
+    button2.innerText = "Go to Cave";
+    button3.innerText = "Fight Dragon";
+    button1.onclick = buyHealth;
+    button2.onclick = buyWeapon;
+    button3.onclick = goTown;
+    text.innerText = "You are in the Town Square. You see a sign that says 'STORE'.";
+}
 function goStore() {
-    console.log("Going to store.")
+    button1.innerText = "Buy 10 health (10 gold)";
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to Town Square";
+    button1.onclick = buyHealth;
+    button2.onclick = buyWeapon;
+    button3.onclick = goTown;
+    text.innerText = "You entered the Store.";
 }
 
 function goCave() {
@@ -35,3 +52,12 @@ function goCave() {
 function fightDragon() {
     console.log("Fighting dragon.")
 }
+
+function buyHealth(){
+
+}
+
+function buyWeapon(){
+
+}
+
