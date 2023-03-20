75e264b5a606c2cb638e4e5306864b035a513be2
joykara <joykarani1@gmail.com>
2023-03-20 17:35:13 +0300

    update weapon fns

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let gold = 50;
 let currentWeapon = 0;
 let fighting;
 let monsterHealth;
-let inventory = ["stick"];
+let inventory = ["Stick"];
 
 const button1 = document.querySelector("#button1")
 // when creating a variable whose value is constant
@@ -36,6 +36,9 @@ const weapons = [
         power: 100
     }
 ];
+
+
+
 const locations = [
     {
         name: "Town Square",
@@ -84,10 +87,6 @@ function goCave() {
     update(locations[2])
 }
 
-function fightDragon() {
-    console.log("Fighting dragon.")
-}
-
 function buyHealth() {
     if (gold >= 10) {
         gold -= 10
@@ -101,17 +100,45 @@ function buyHealth() {
 
 }
 
-function buyWeapon(){
-    if (gold >= 30) {
-        gold -= 30
-        currentWeapon++; //increase by 1
+// ---------  WEAPONS   --------
+
+function buyWeapon() {
+    if (currentWeapon < weapons.length - 1){
+        if (gold >= 30) {
+            gold -= 30
+            currentWeapon++; //increase by 1
+            goldText.innerText = gold;
+            let newWeapon = weapons[currentWeapon].name;
+            text.innerText = "You now have a " + newWeapon + ".";
+            inventory.push(newWeapon);
+            text.innerText += " In your inventory you have: " + inventory;  //update inventory
+        }
+        else {
+            text.innerText("Not enough gold to buy a weapon.")
+        }
+    }
+    else {
+        text.innerText("You already have the most powerful weapon.")
+        button2.onclick = "Sell weapon for 15 gold";
+        button2.onclick = sellWeapon;
+    }
+
+}
+
+function sellWeapon() {
+    if (inventory.length > 1) {
+        gold += 15
         goldText.innerText = gold;
-        let newWeapon = weapons[currentWeapon].name;
-        text.innerText = "You now have a " + newWeapon + ".";
-        inventory.push(newWeapon);
-        text.innerText = "In your inventory you have: " + inventory;
+        let currentWeapon = inventory.shift();
+        text.innerText = "You sold a " + currentWeapon + ".";
+        text.innerText += " In your inventory you have: " + inventory; //update inventory
+    }
+    else {
+        text.innerText = "Don't sell your only weapon!"
     }
 }
+
+// ------------ FIGHTING MONSTERS ------------
 
 function fightSlime() {
     
@@ -119,6 +146,10 @@ function fightSlime() {
 
 function fightBeast() {
     
+}
+
+function fightDragon() {
+    console.log("Fighting dragon.")
 }
 
 
